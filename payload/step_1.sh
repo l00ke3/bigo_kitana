019a06954f960bcba40db165a00db526fb4c664a
dev01team <125609175+dev01team@users.noreply.github.com>
2023-02-27 05:52:51 +0100

    Update step_1.sh

--- payload/step_1.sh
+++ payload/step_1.sh
@@ -9,7 +9,8 @@ DEBIAN_FRONTEND=noninteractive apt-get -f install  -y \
         software-properties-common python3 python3-dev python3-numpy \
         openvpn autocutsel  python3-pip python3-tk python3-dev  pwgen  \
         wget openssh-server locate nano gedit screen  net-tools curl git tor supervisor \
-        xvfb xserver-xephyr ttf-wqy-zenhei python2.7 supervisor asciinema
+        asciinema
+        #xvfb xserver-xephyr ttf-wqy-zenhei python2.7  asciinema
         #xfce4-goodies xfce4 \
         #thunar tumbler xarchiver \
         #mugshot thunar-archive-plugin \
